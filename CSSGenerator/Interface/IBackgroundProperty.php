4044ce6ce9859b5f749f1d3d43dd3cf5c2378c03
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-04-18 09:12:22 +0800

    fix to be recognize as php
    added <?php on top to be valid php code

--- CSSGenerator/Interface/IBackgroundProperty.php
+++ CSSGenerator/Interface/IBackgroundProperty.php
@@ -1,3 +1,5 @@
+<?php
+
 interface IBackgroundProperty{
     public function setColor(string $color = "#FDFDFD"): IBackgroundProperty
     public function setImage_bg(string $imgDir): IBackgroundProperty
--- CSSGenerator/Interface/IColorProperty.php
+++ CSSGenerator/Interface/IColorProperty.php
@@ -1,3 +1,5 @@
+<?php
+
 interface IColorProperty{
     public function setColor(string $color = "#010101"): IColorProperty;
     public function setOpacity(string $opacity = "1"): IColorProperty;
