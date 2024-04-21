a19a7c390f088676fd8c08e30fea5ef772a6c909
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-04-21 19:14:26 +0800

    added interface for export to file
    function for exporting to file css

--- CSSGenerator/Interface/ICSSExport.php
+++ CSSGenerator/Interface/ICSSExport.php
@@ -2,5 +2,7 @@
 namespace Generator\CSS\Interface;
 interface ICSSExport{
 	public function getCSStext(): string;
+	public function put2file(string $filedir = "css.file", bool $isAppend = true, bool $isCustomDir = false): bool;
+	public function mkfile(): bool;
 }
 
