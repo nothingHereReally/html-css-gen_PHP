e39c23463bb206b841c602177879f32ddb69c2a2
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-05-02 12:08:50 +0800

    added test for css generator
    initial testing phase

---
+++ test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Site title</title>
+	  <meta charset="utf-8">
+	  <meta name="description" content="test website test blah">
+	  <link rel="icon" href="favicon.ico" type="image/x-icon">
+	  <style type="text/css">
+		*{
+			color: #EEEEEE;
+			background-color: #111111;
+		}
+	  </style>
+   </head>
+   <body>
+      <h1>hello world</h1>
+      <hr>
+      <p>hello I am someone, blah. I'm currently learning how to code in php
+         as my main backend programming tool. I like to watch movies, series,
+         and this is just a test for the css Generator or library that will
+         be written in php.
+      </p>
+      <hr>
+   </body>
+</html>
