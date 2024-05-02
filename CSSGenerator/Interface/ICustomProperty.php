d85cd003fe59a4c59df0151c2a0ddb60546bed06
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-05-02 13:19:36 +0800

    added interface public function for altering values
    this is to prevent overwrite on css property value pair

--- CSSGenerator/Interface/ICustomProperty.php
+++ CSSGenerator/Interface/ICustomProperty.php
@@ -3,5 +3,7 @@ namespace Generator\CSS\Interface;
 interface ICustomProperty{
     public function addProp(string $property, string $value): ICustomProperty;
     public function addMultiProp(array $keyProp_valValue): ICustomProperty;
+    public function alterProp(string $property, string $value): ICustomProperty;
+    public function getPropValues(string $property): string;
 }
 
