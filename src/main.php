f063385fd784639b3c28e1cdcc3cc478e7876913
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2024-05-17 20:21:15 +0800

    added some instruction to output file
    instruction added: going out of src(../) to outdir(../outdir), then file itself(../outdir/file.extension)

--- src/main.php
+++ src/main.php
@@ -2,4 +2,8 @@
 require_once "../autoloader.php";
 require_once "./libmenu.php";
 require_once "./libretto.php";
+/*
+ * directory output string: "../outdir/"
+ * example: "../outdir/libretto.html"
+ */
 
